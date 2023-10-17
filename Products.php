0cedc807ad10d36941270baa7167a1bb8cca31d2
DiyanBhudiya <146846891+DiyanBhudiya@users.noreply.github.com>
2023-10-17 15:18:23 +0100

    Create Diyan branch

--- Products.php
+++ Products.php
@@ -1,4 +1,4 @@
-<h1>Hello world!</h1<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -120,86 +120,6 @@
     </nav>
 
     <!-- Carousel -->
-    <div
-      id="hero-carousel"
-      class="carousel slide carousel-fade"
-      data-bs-ride="carousel"
-    >
-      <div class="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#hero-carousel"
-          data-bs-slide-to="0"
-          class="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#hero-carousel"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#hero-carousel"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-
-      <div class="carousel-inner">
-        <div class="carousel-item active c-item">
-          <img
-            src="assets/radiohead-bundle.png"
-            class="d-block c-img"
-            alt="Slide 1"
-          />
-          <!--    <div class="carousel-caption d-none d-md-block">
-            <h5 style="background-color: var(--accentBlack)">
-              RadioHead - In rainbows
-            </h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>-->
-        </div>
-        <div class="carousel-item c-item">
-          <img
-            src="assets/forallthedogs-album.png"
-            class="d-block w-100 c-img"
-            alt="Slide 2 "
-          />
-        </div>
-        <div class="carousel-item c-item">
-          <img
-            src="assets/radiohead-amn.jpg"
-            class="d-block c-img"
-            alt="Slide 3"
-          />
-        </div>
-      </div>
-
-      <!-- Carousel buttons -->
-
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#hero-carousel"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#hero-carousel"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
