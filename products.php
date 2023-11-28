b414fbbe2d38c26140d05b7672684e4a42d5beee
ByrneHub <116261123+ByrneHub@users.noreply.github.com>
2023-11-28 10:08:09 +0000

    Just for laptop work

--- products.php
+++ products.php
@@ -16,7 +16,7 @@
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   </head>
 
-  <body class="">
+  <body id="products-page">
   <!-- nav start -->
   <div>
 
