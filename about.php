3c87b2a5c59c143d31fe5934282bb068730dc256
ByrneHub <116261123+ByrneHub@users.noreply.github.com>
2023-12-09 17:56:08 +0000

    Footer Added

--- about.php
+++ about.php
@@ -254,7 +254,7 @@ session_start()
 
   <!--   Logo Img       -->
    <div class="about-us-img col-md-4 align-items-center mx-auto " >
-  <img src="assets/logo.png"  >
+  <img src="assets/logo.png"/>
   </div>
 
     <!-- Contact Information -->
@@ -269,7 +269,97 @@ session_start()
       </div>
     </div>
 
+  <footer id="rest-footer">
+      <div class="footer-top">
+          <div class="row">
+              <div class="col-lg-3 col-md-6  align-items-center text-center">
+                  <br>
+                  <br>
+                  <a  href="about.php" class="align-items-center" >
+                      <img src="assets/logo.png" width="100px" >
+                  </a>
+                  <br>
+                  <br>
 
+              </div>
+              <div class="col-lg-2 col-md-6 footer-links">
+                  <br>
+
+                  <h4 class="text-center" id="footer-useful"> Useful Links</h4>
+
+                  <ul class="footer-list">
+
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="index.php" class="footer-items "> Home</a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="products.php" class="footer-items"> Products page</a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="about.php" class="footer-items"> Who we are!</a>
+                      </li>
+                  </ul>
+
+
+              </div>
+              <div class="col-lg-2 col-md-6 footer-contact">
+                  <br>
+
+                  <h4 class="text-center" id="footer-contact-us"> Contact Us</h4>
+                  <ul class="footer-list">
+
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="mailto:info@cadence.com" class="footer-items "> Email  <span id="footer-email-color">info@cadence.com </span></a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="#" class="footer-items"> Privacy Policy</a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="#" class="footer-items"> Cookie Policy</a>
+                      </li>
+                  </ul>
+              </div>
+
+              <div class="col-lg-2 col-md-2 footer-contact">
+                  <br>
+
+                  <h4 class="text-center" id="footer-contact-us"> Legal</h4>
+
+                  <ul class="footer-list">
+
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="#" class="footer-items "> Modern Slavery Statement</a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="#" class="footer-items"> Privacy Policy</a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="#" class="footer-items"> Cookie Policy</a>
+                      </li>
+                      <li>
+                          <i class="fa fa"></i>
+                          <a href="#" class="footer-items"> Terms & Condition</a>
+                      </li>
+                  </ul>
+
+              </div>
+          </div>
+
+          <div id="footer-copyright" >
+              <h5> 2023 @Copyright Cadence </h5>
+
+          </div>
+
+  </footer>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
